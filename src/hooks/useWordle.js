c94c16a66f1d6eaf063f7392db4e9533423b67f1
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-29 17:33:57 -0500

    Prevent invalid guess submission

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -4,13 +4,15 @@ const useWordle = (solution) => {
   const [turn, setTurn] = useState(0);
   const [currentGuess, setCurrentGuess] = useState("");
   const [guesses, setGuesses] = useState([]);
-  const [history, setHistory] = useState([]);
+  const [history, setHistory] = useState(['ninja']);
   const [isCorrect, setIsCorrect] = useState(false);
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'b', color: 'green'}]
 
-  const formatGuess = () => {};
+  const formatGuess = () => {
+    console.log("current guess is: " + currentGuess);
+  };
 
   // add a new guess to the guesses state
   // update the isCorrect state if the guess is correct
@@ -22,6 +24,29 @@ const useWordle = (solution) => {
   // if user presses enter, add the new guess
 
   const handleKeyup = ({ key }) => {
+    // check if key is enter
+    if (key === "Enter") {
+      // only add guess if turn is less than 5
+      if (turn > 5) {
+        console.log("you used all your guesses");
+        return;
+      }
+
+      // do not allow duplicate words
+      if (history.includes(currentGuess)) {
+        console.log("you already tried that word");
+        return;
+      }
+
+      // check word is 5 chars long
+      if (currentGuess.length !== 5) {
+        console.log("word must be 5 chars long");
+        return;
+      }
+
+      formatGuess();
+    }
+
     // check if key is backspace
     if (key === "Backspace") {
       setCurrentGuess((prev) => {
