da1dde4e2bd3309b2393231fec17197ecfd6953a
Jonathan <114364831+jjlin526@users.noreply.github.com>
2022-12-30 15:12:18 -0500

    Color used keys on keypad

--- src/components/Wordle.js
+++ src/components/Wordle.js
@@ -1,30 +1,29 @@
-import React, { useEffect } from "react";
-import useWordle from "../hooks/useWordle";
-import Grid from "./Grid";
-import Keypad from "./Keypad";
+import React, { useEffect } from 'react'
+import useWordle from '../hooks/useWordle'
 
-// all UI of game
+// components
+import Grid from './Grid'
+import Keypad from './Keypad'
 
 export default function Wordle({ solution }) {
-  const { currentGuess, handleKeyup, guesses, isCorrect, turn } =
-    useWordle(solution);
+  const { currentGuess, guesses, turn, isCorrect, usedKeys, handleKeyup } = useWordle(solution)
 
   useEffect(() => {
-    window.addEventListener("keyup", handleKeyup);
+    window.addEventListener('keyup', handleKeyup)
 
-    return () => window.removeEventListener("keyup", handleKeyup);
-  }, [handleKeyup]);
+    return () => window.removeEventListener('keyup', handleKeyup)
+  }, [handleKeyup])
 
   useEffect(() => {
-    console.log(guesses, turn, isCorrect);
-  }, [guesses, turn, isCorrect]);
+    console.log(guesses, turn, isCorrect)
+  }, [guesses, turn, isCorrect])
 
   return (
     <div>
-      <div>solution: {solution}</div>
-      <div>current guess: {currentGuess}</div>
-      <Grid currentGuess={currentGuess} guesses={guesses} turn={turn} />
-      <Keypad />
+      <div>solution - {solution}</div>
+      <div>Current Guess - {currentGuess}</div>
+      <Grid guesses={guesses} currentGuess={currentGuess} turn={turn} />
+      <Keypad usedKeys={usedKeys} />
     </div>
-  );
+  )
 }
--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -6,6 +6,7 @@ const useWordle = (solution) => {
   const [guesses, setGuesses] = useState([...Array(6)]); // each guess is an array
   const [history, setHistory] = useState([]); // each guess is a string
   const [isCorrect, setIsCorrect] = useState(false);
+  const [usedKeys, setUsedKeys] = useState({});
 
   // format a guess into an array of letter objects
   // e.g. [{key: 'a', color: 'yellow'}]
@@ -55,6 +56,28 @@ const useWordle = (solution) => {
       return prevTurn + 1;
     });
 
+    setUsedKeys((prevUsedKeys) => {
+      formattedGuess.forEach((l) => {
+        const currentColor = prevUsedKeys[l.key];
+        if (l.color === "green") {
+          prevUsedKeys[l.key] = "green";
+          return;
+        }
+        if (l.color === "yellow" && currentColor !== "green") {
+          prevUsedKeys[l.key] = "yellow";
+          return;
+        }
+        if (
+          l.color === "grey" &&
+          currentColor !== "green" &&
+          currentColor !== "yellow"
+        ) {
+          prevUsedKeys[l.key] = "grey";
+          return;
+        }
+      });
+      return prevUsedKeys;
+    });
     setCurrentGuess("");
   };
 
@@ -91,7 +114,7 @@ const useWordle = (solution) => {
     }
   };
 
-  return { turn, currentGuess, guesses, isCorrect, handleKeyup };
+  return { turn, currentGuess, guesses, isCorrect, usedKeys, handleKeyup };
 };
 
 export default useWordle;
